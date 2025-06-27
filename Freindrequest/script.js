6debc5a1e92eeb0789e1896b3bc2624c153b5918
affanTarique <reply2affankhan@gmail.com>
2025-06-28 01:19:53 +0530

    more feautre

--- Freindrequest/script.js
+++ Freindrequest/script.js
@@ -1,8 +1,17 @@
 var istatus = document.querySelector("h5")
-var addFreind= document.querySelector("#add")
+var addFriend= document.querySelector("#add")
+var removeFriend = document.querySelector("#remove")
+
 
 addFreind.addEventListener("click", function () {
     istatus.innerHTML = "Friends"
     istatus.style.color = "green"
+
+})
+
+removeFriend.addEventListener("click", function () {
+    istatus.innerHTML = "Suggestion"
+    istatus.style.color = "red"
+
 
 })
