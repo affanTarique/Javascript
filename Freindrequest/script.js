c2f77817a4a7cbf43ec271ad87093e1f4c3fd25c
affanTarique <reply2affankhan@gmail.com>
2025-06-28 01:10:57 +0530

    my js file

---
+++ Freindrequest/script.js
@@ -0,0 +1,8 @@
+var istatus = document.querySelector("h5")
+var addFreind= document.querySelector("#add")
+
+addFreind.addEventListener("click", function () {
+    istatus.innerHTML = "Friends"
+    istatus.style.color = "green"
+
+})
