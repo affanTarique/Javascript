965e0c5307b83877db5e76194ed91d100390cd8d
affanTarique <reply2affankhan@gmail.com>
2025-06-28 01:43:00 +0530

    new changes

--- Freindrequest/script.js
+++ Freindrequest/script.js
@@ -1,17 +1,18 @@
 var istatus = document.querySelector("h5")
 var addFriend= document.querySelector("#add")
-var removeFriend = document.querySelector("#remove")
-
+var check = 0
 
 addFreind.addEventListener("click", function () {
+    if(check ==0) {
+        istatus.innerHTML = "Friends"
+        istatus.style.color = "green"
+
+
+    } else {
+
+    
     istatus.innerHTML = "Friends"
     istatus.style.color = "green"
-
-})
-
-removeFriend.addEventListener("click", function () {
-    istatus.innerHTML = "Suggestion"
-    istatus.style.color = "red"
-
+    }
 
 })
