d2d1e00cbcac1d1ad29b58de0996b9a4d39ef7f9
affanTarique <reply2affankhan@gmail.com>
2025-07-03 11:19:47 +0530

    new js

--- Revision/JSanswer.js
+++ Revision/JSanswer.js
@@ -11,3 +11,18 @@ var first ="Affan"
 var last = "Khan"
 console.log(first + " " + last);
 console.log(`${first} ${last}`);
+
+var a = 40;
+console.log(a);
+a= 50;
+console.log(a);
+
+
+console.error("something went wrong");
+
+console.log(12*12);
+
+var a = true;
+var b = false;
+console.log(typeof a);
+console.log(typeof b);
