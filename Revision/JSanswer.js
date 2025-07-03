a429065b2cea2193df6fa4128e24983ff110f549
affanTarique <reply2affankhan@gmail.com>
2025-07-03 10:55:37 +0530

    add var

--- Revision/JSanswer.js
+++ Revision/JSanswer.js
@@ -5,3 +5,9 @@ console.log(45*2-10);
 
 const todaydate = new Date ();
 console.log(todaydate.getFullYear());
+
+
+var first ="Affan"
+var last = "Khan"
+console.log(first + " " + last);
+console.log(`${first} ${last}`);
