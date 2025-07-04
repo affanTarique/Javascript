9255b0c2297fb0f12854cec48b7541a8723ca05e
affanTarique <reply2affankhan@gmail.com>
2025-07-05 01:25:14 +0530

    vs

--- Revision/JSanswer.js
+++ Revision/JSanswer.js
@@ -1,32 +1,44 @@
+//1. Log your name and favorite hobby to the console.//
 console.log("Affan khan" , "I love to build");
 
-
+//2. Perform and log the result of 45 \* 2 - 10.//
 console.log(45*2-10);
 
+
+//3. Use `console.log()` to display the current year.//
 const todaydate = new Date ();
 console.log(todaydate.getFullYear());
 
-
+//4. Create two variables for first and last name. Concatenate and log them.//
 var first ="Affan"
 var last = "Khan"
 console.log(first + " " + last);
 console.log(`${first} ${last}`);
 
+
+
+//5. Track the value of a variable by logging it before and after updating.//
 var a = 40;
 console.log(a);
 a= 50;
 console.log(a);
 
-
+//6. Use `console.error()` to simulate an error message.//
 console.error("something went wrong");
 
+
+
+//7. Log the square of the number 12 to the console.//
 console.log(12*12);
 
+//8. Print the type of a variable holding the value true.//
 var a = true;
 var b = 59;
 console.log(typeof a);
 console.log(typeof b);
 
+
+// . Create a variable holding your age and log whether it’s greater than 18.//
 var age = 32;
 if (age >=18) {
     console.log("You are an adult");
@@ -34,9 +46,20 @@ if (age >=18) {
     console.log("You are not an adult");
 }
 
+
+// Log the result of 100 / 0 and observe the output.//
+
 console.log(100/0);
 
+
+
+
+
+
+
 /*var and data types*/
+
+//11. Declare a variable using let and log its value.//
 let c = 82;
 console.log(c);
 
