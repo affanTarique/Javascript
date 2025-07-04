476e7b01bc798d53f8e7d43065b00a13cd8cf68d
affanTarique <reply2affankhan@gmail.com>
2025-07-04 21:06:36 +0530

    add all files

---
+++ Revision/JSanswer.js
@@ -0,0 +1,49 @@
+console.log("Affan khan" , "I love to build");
+
+
+console.log(45*2-10);
+
+const todaydate = new Date ();
+console.log(todaydate.getFullYear());
+
+
+var first ="Affan"
+var last = "Khan"
+console.log(first + " " + last);
+console.log(`${first} ${last}`);
+
+var a = 40;
+console.log(a);
+a= 50;
+console.log(a);
+
+
+console.error("something went wrong");
+
+console.log(12*12);
+
+var a = true;
+var b = 59;
+console.log(typeof a);
+console.log(typeof b);
+
+var age = 32;
+if (age >=18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are not an adult");
+}
+
+console.log(100/0);
+
+/*var and data types*/
+let c = 82;
+console.log(c);
+
+const P1 = Math.PI;
+console.log(P1);
+
+
+let x = 16;
+x = 46;
+console.log(x);
