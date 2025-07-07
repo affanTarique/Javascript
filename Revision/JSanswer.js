905ec576eca6642f81fdf89b7693e9a86279bb91
affanTarique <reply2affankhan@gmail.com>
2025-07-07 23:42:05 +0530

    js

--- Revision/JSanswer.js
+++ Revision/JSanswer.js
@@ -144,5 +144,7 @@ console.log(sum);
 
 //23.Create a for ...of loop to log each character of the straing "JavaScript".
 let str = "JavaScript";
-
+for (let character of str) {
+    console.log(character);
+}
 
